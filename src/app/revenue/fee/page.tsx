fa72891861501440deb6b49114d11077f2941b07
h470h <hassaanazam7@gmail.com>
2024-07-29 13:32:53 +0500

    ClassDeletionMerge

--- src/app/revenue/fee/page.tsx
+++ src/app/revenue/fee/page.tsx
@@ -4,7 +4,7 @@ export default function Fee() {
     <main className="min-h-screen sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3 bg-yellow-100/50">
     <div className="container mx-auto p-4">
     <div className="relative overflow-hidden">
-      <h1 className="text-center text-5xl font-serif font-bold tracking-tight p-2git">
+      <h1 className="text-center text-5xl font-serif font-bold tracking-tight p-2">
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-600 to-yellow-500 hover:from-orange-400 hover:to-green-500 transition-all duration-300">
           Fee Collection
         </span>
